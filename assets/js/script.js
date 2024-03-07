4863029ee6b8a8669d65d565fb8d01c17ee10f81
Josue Gallardo <dotelias02@gmail.com>
2024-03-07 16:42:37 -0300

    Termino de Desafío

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,63 @@
+/* Se crea variable n para el número de partidas */
 let n = parseInt(prompt("¿Cuantas veces quieres jugar?"));
 let contadorPlayer = 0;
 let contadorCPU = 0;
 
+/* Se crea un bucle para el número de partidas */
 for (let i = 1; i <= n; i++) {
   let jugada = parseInt(
     prompt("Ingresa 0 para Piedra, 1 para Papel o 2 para Tijera")
   );
   let jugadaCPU = Math.floor(Math.random() * 3);
-  if (jugadaCPU == jugada) {
-    document.write("Empataste con la CPU! <br>");
-  } else if (jugadaCPU == 0 && jugada == 1) {
-    document.write("La CPU jugó Piedra. Ganaste, Felicitaciones! <br>");
-    contadorPlayer++;
-  } else if (jugadaCPU == 0 && jugada == 2) {
-    document.write("La CPU jugó Piedra. Perdiste, la CPU te gano <br>");
-    contadorCPU++;
-  } else if (jugadaCPU == 1 && jugada == 0) {
-    document.write("La CPU jugó Papel. Perdiste, la CPU te gano <br>");
-    contadorCPU++;
-  } else if (jugadaCPU == 1 && jugada == 2) {
-    document.write("La CPU jugó Papel. Ganaste, Felicitaciones! <br>");
-    contadorPlayer++;
-  } else if (jugadaCPU == 2 && jugada == 0) {
-    document.write("La CPU jugó Tijera. Ganaste, Felicitaciones! <br>");
-    contadorPlayer++;
-  } else if (jugadaCPU == 2 && jugada == 1) {
-    document.write("La CPU jugó Tijera. Perdiste, la CPU te gano <br>");
-    contadorCPU++;
-  } else {
-    document.write("Ingresa un valor valido, intenta de nuevo <br>");
-    i--;
+  switch (jugada) {
+    case jugadaCPU:
+      document.write("Empataste con la CPU! <br>");
+      break;
+    case 0:
+      if (jugadaCPU === 1) {
+        document.write("La CPU jugó Papel. Perdiste, la CPU te gano <br>");
+        contadorCPU++;
+      } else if (jugada === 2) {
+        document.write("La CPU jugó Tijera. Ganaste, Felicitaciones! <br>");
+        contadorPlayer++;
+      }
+      break;
+    case 1:
+      if (jugadaCPU === 0) {
+        document.write("La CPU jugó Piedra. Ganaste, Felicitaciones! <br>");
+        contadorPlayer++;
+      } else if (jugada === 2) {
+        document.write("La CPU jugó Tijera. Perdiste, la CPU te gano <br>");
+        contadorCPU++;
+      }
+      break;
+    case 2:
+      if (jugadaCPU === 0) {
+        document.write("La CPU jugó Piedra. Perdiste, la CPU te gano <br>");
+        contadorCPU++;
+      } else if (jugadaCPU === 1) {
+        document.write("La CPU jugó Papel. Tijera. <br>");
+        contadorPlayer++;
+      }
+      break;
+    default:
+      document.write("Ingresa un valor valido, intenta de nuevo <br>");
+      i--;
+      break;
   }
 }
+
+/* Se crea un condicional para el resultado de las partidas */
 if (contadorPlayer > contadorCPU) {
-  document.write(`<br> De ${n} partidas ganaste ${contadorPlayer} veces y la CPU ${contadorCPU}, felicitaciones ganaste!.`);
+  document.write(
+    `<br> De ${n} partidas ganaste ${contadorPlayer} y la CPU ${contadorCPU}, felicitaciones ganaste!.`
+  );
 } else if (contadorPlayer < contadorCPU) {
-  document.write(`<br> De ${n} partidas ganaste ${contadorPlayer} veces y la CPU ${contadorCPU}, la CPU te gano, perdiste!.`);
+  document.write(
+    `<br> De ${n} partidas ganaste ${contadorPlayer} y la CPU ${contadorCPU}, la CPU te gano, perdiste!.`
+  );
 } else {
-  document.write(`<br> De ${n} partidas ganaste ${contadorPlayer} veces y la CPU ${contadorCPU}, empate!.`);
+  document.write(
+    `<br> De ${n} partidas ganaste ${contadorPlayer} y la CPU ${contadorCPU}, empate!.`
+  );
 }
